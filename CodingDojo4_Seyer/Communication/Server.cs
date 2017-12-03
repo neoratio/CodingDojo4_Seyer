2f67dbd83a07a8bffe41e3ee2eb3dd8c9e22c632
neoratio <stefan_seyer@hotmail.com>
2017-12-03 12:17:26 +0100

    WBF Server, Server Klasse Update

--- CodingDojo4_Seyer/Communication/Server.cs
+++ CodingDojo4_Seyer/Communication/Server.cs
@@ -1,12 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CodingDojo4_Seyer.Communication
 {
     class Server
     {
+        Socket SSocket;
+        List<ClientHandler> CL = new List<ClientHandler>();
+        Action<string> GU;
+        Thread AcceptingTask;
+
+        public Server (String IP, int Port, Action<string> GU)
+        {
+            this.GU = GU;
+            SSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+            SSocket.Bind(new IPEndPoint(IPAddress.Parse(IP), Port));
+            SSocket.Listen(5);
+        }
+
+        public void StartServer()
+        {
+            AcceptingTask = new Thread(new ThreadStart(Accepting));
+            AcceptingTask.IsBackground = true;
+            AcceptingTask.Start();
+        }
+
+        public void StopServer()
+        {
+            SSocket.Close();
+            AcceptingTask.Abort(); 
+            foreach (var item in CL)
+            {
+                item.Close();
+            }
+            CL.Clear();
+
+        }
+
+        private void Accepting()
+        {
+            while (AcceptingTask.IsAlive)
+            {
+                try
+                {
+                    CL.Add(new ClientHandler(SSocket.Accept(), new Action<string, Socket>(NewMessageReceived)));
+                }
+                catch (Exception e)
+                {
+                  
+                }
+            }
+        }
+
+        private void NewChatMessage(string message, Socket senderSocket)
+        {
+            //update gui
+            GU(message);
+            //write message to all clients
+            foreach (var item in CL)
+            {
+                if (item.Clientsocket != senderSocket)
+                {
+                    item.Send(message);
+                }
+            }
+
+        }
+
+        public void DisconnectClient(string name)
+        {
+            foreach (var item in CL)
+            {
+                if (item.Name.Equals(name))
+                {
+                    item.Close();
+                    CL.Remove(item);
+                    break;
+                }
+            }
+        }
+
+
+
     }
 }
