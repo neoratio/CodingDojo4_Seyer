9f7b28da48a637e89668c6705581b0973d39ffb6
neoratio <stefan_seyer@hotmail.com>
2017-12-03 21:08:25 +0100

    Solution Changed

--- Server/ViewModel/ViewModelLocator.cs
+++ Server/ViewModel/ViewModelLocator.cs
@@ -1,4 +1,16 @@
+/*
+  In App.xaml:
+  <Application.Resources>
+      <vm:ViewModelLocator xmlns:vm="clr-namespace:Server"
+                           x:Key="Locator" />
+  </Application.Resources>
+  
+  In the View:
+  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
 
+  You can also use Blend to do all this with the tool's support.
+  See http://www.galasoft.ch/mvvm
+*/
 
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
